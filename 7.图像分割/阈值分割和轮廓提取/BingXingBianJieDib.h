26845de290d272f1bc06e21f1578d37dab5e8d69
xin liu <xinliuchnsz@gmail.com>
2019-11-22 23:34:43 +0800

    7.1通过阈值进行图像分割

---
+++ 7.图像分割/阈值分割和轮廓提取/BingXingBianJieDib.h
@@ -0,0 +1,44 @@
+#ifndef __BingXingBianJieDib_H
+#define __BingXingBianJieDib_H
+#include "Cdib.h"
+ 
+typedef struct{
+    int Value;
+    int Dist;
+    int AngleNumber;
+}    MaxValue;
+
+typedef struct{
+    int Height;
+    int Width;
+}    Seed;
+
+typedef struct{
+    int Height;
+    int Width;
+}    Point;
+
+class BingXingBianJieDib :public CDib
+{
+
+public:
+    BingXingBianJieDib();
+    ~BingXingBianJieDib();
+
+public:
+    void Lunkuogenzong(CPoint SeedPoint);
+    void Lunkuotiqu(CPoint SeedPoint);
+    void Qiyuzengzhang(CPoint point);
+    void Zhongzitianchong(CPoint SeedPoint);
+    void Lunkuogenzong();
+    void Lunkuotiqu();
+    void Lunkuotiqu2();
+    void BanYuZhi(int Yuzhi);  // 阈值或半阈值分割法，需要人工根据灰度直方图选取阈值（一般为波谷），分割前景和背景
+    void Yuzhifenge(int Yuzhi);
+    void Diedaifazhi(); // 迭代阈值法，指定初始阈值，由程序自动迭代计算阈值
+    void Fenbutongji(int *tongji);
+    void Zhifangtu(float *tongji);
+
+protected:
+};
+#endif
